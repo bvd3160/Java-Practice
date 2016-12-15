65a76ec45be94539dcc67edbcdfc1d7812305b11
Jean-Yves Kwibuka <jkwibuka@yahoo.co.nz>
2016-12-15 19:54:56 +1300

    Hello World

--- src/Java/AttributesOrVariables.java
+++ src/Java/AttributesOrVariables.java
@@ -1,5 +1,11 @@
 package Java;
 
+import javax.print.attribute.standard.PrinterLocation;
+
 public class AttributesOrVariables {
+	
+	public static void main(String args[]){
+		System.out.println("Hello World");
+	}
 
 }
